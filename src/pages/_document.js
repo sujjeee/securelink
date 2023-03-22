6758b37e8ac1d4ec642cd11b5254f7d8536cb22e
Suraj <surajgupta.college@gmail.com>
2023-03-22 16:17:32 +0530

    fixed favicon issue

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,8 +8,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="shortcut icon" href="https://securelink.vercel.app/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
 
         <meta name="description" content={description} />
 
