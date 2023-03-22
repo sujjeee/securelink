8dbbf971aaee473bbe21ad62747306deca5b4173
Suraj <surajgupta.college@gmail.com>
2023-03-22 13:48:39 +0530

    added analytics and google meta tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,38 @@
 import Navbar from '@/components/Navbar'
 import { store } from '@/redux/store'
 import '@/styles/globals.css'
+import Script from 'next/script'
 import { Provider } from 'react-redux'
 
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <div className="flex flex-col min-h-screen">
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Provider store={store}>
+        <div className="flex flex-col min-h-screen">
 
-        <Component {...pageProps} />
-      </div>
-    </Provider>
+          <Component {...pageProps} />
+        </div>
+      </Provider>
+    </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import GetLink from '@/components/GetLink';
 import Hero from '@/components/Hero';
 import Navbar from '@/components/Navbar';
 import ShortLinks from '@/components/ShortLinks';
+import Head from 'next/head';
 
 const Home = () => {
     const [urls, setUrls] = useState([]);
@@ -53,44 +54,50 @@ const Home = () => {
     }, [filterUrls]);
 
     return (
-        <div>
-            <header className='border-b border-[#333333] sticky top-0  backdrop-blur-3xl z-50'>
-                <Navbar />
-            </header>
-            <Hero />
-            <GetLink />
-            <div className='pb-28 py-24 sm:py-40 sm:pb-0 px-4' >
-                <div className="mx-auto text-center py-9 sm:py-16 max-w-[320px] sm:max-w-4xl">
-                    <h1 className="mx-auto  font-display text-4xl font-bold tracking-normal text-gray-300 sm:text-5xl text-transparent bg-gradient-to-t bg-clip-text from-zinc-100/50 to-white ">
-                        Generated links
-                    </h1>
+        <>
+            <Head>
+                <title>SecureLinks</title>
+                <meta name="google-site-verification" content="5z2lDnQ6mdG9S2qZm74DNfOk3xdwLR-orzDHc5XiJxs" />
+            </Head>
+            <div>
+                <header className='border-b border-[#333333] sticky top-0  backdrop-blur-3xl z-50'>
+                    <Navbar />
+                </header>
+                <Hero />
+                <GetLink />
+                <div className='pb-28 py-24 sm:py-40 sm:pb-0 px-4' >
+                    <div className="mx-auto text-center py-9 sm:py-16 max-w-[320px] sm:max-w-4xl">
+                        <h1 className="mx-auto  font-display text-4xl font-bold tracking-normal text-gray-300 sm:text-5xl text-transparent bg-gradient-to-t bg-clip-text from-zinc-100/50 to-white ">
+                            Generated links
+                        </h1>
 
-                </div>
-                <div className='grid justify-center mx-auto space-y-3'>
-                    <ShortLinks shortedLinks={gitUrl} />
-                    {urls.map((url, index) => (
-                        <div key={index} className="flex">
-                            <ShortLinks shortedLinks={url} />
-                        </div>
-                    ))}
+                    </div>
+                    <div className='grid justify-center mx-auto space-y-3'>
+                        <ShortLinks shortedLinks={gitUrl} />
+                        {urls.map((url, index) => (
+                            <div key={index} className="flex">
+                                <ShortLinks shortedLinks={url} />
+                            </div>
+                        ))}
 
+                    </div>
+                </div >
+                <div className='pb-28 py-20 sm:py-32' >
+                    <div className="mx-auto text-center py-9 sm:py-16 max-w-[320px] sm:max-w-4xl">
+                        <h1 className="mx-auto  font-display text-4xl font-bold tracking-normal text-gray-300 sm:text-5xl text-transparent bg-gradient-to-t bg-clip-text from-zinc-100/50 to-white py-4">
+                            Powerful Access Control
+                        </h1>
+
+                    </div>
+                    <div >
+                        <Features />
+                    </div>
+                </div >
+                <div className="mx-auto max-w-2xl text-center font-display text-2xl font-extrabold tracking-normal text-gray-300 sm:text-5xl pb-24 text-transparent bg-gradient-to-t bg-clip-text from-zinc-100/50 to-white ">
+                    Protect your links now!
                 </div>
             </div >
-            <div className='pb-28 py-20 sm:py-32' >
-                <div className="mx-auto text-center py-9 sm:py-16 max-w-[320px] sm:max-w-4xl">
-                    <h1 className="mx-auto  font-display text-4xl font-bold tracking-normal text-gray-300 sm:text-5xl text-transparent bg-gradient-to-t bg-clip-text from-zinc-100/50 to-white py-4">
-                        Powerful Access Control
-                    </h1>
-
-                </div>
-                <div >
-                    <Features />
-                </div>
-            </div >
-            <div className="mx-auto max-w-2xl text-center font-display text-2xl font-extrabold tracking-normal text-gray-300 sm:text-5xl pb-24 text-transparent bg-gradient-to-t bg-clip-text from-zinc-100/50 to-white ">
-                Protect your links now!
-            </div>
-        </div >
+        </>
     )
 }
 
